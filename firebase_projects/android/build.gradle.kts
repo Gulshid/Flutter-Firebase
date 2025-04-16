3f47ec1d4cb69dd43ef72bf1d460eaf738b2cc5c
Gulshid Zada <gulshidzada4@gmail.com>
2025-04-16 23:13:29 +0500

    Firebase working

--- firebase_projects/android/build.gradle.kts
+++ firebase_projects/android/build.gradle.kts
@@ -8,13 +8,13 @@ allprojects {
 val newBuildDir: Directory = rootProject.layout.buildDirectory.dir("../../build").get()
 rootProject.layout.buildDirectory.value(newBuildDir)
 
-plugins {
-  // ...
+// plugins {
+//   // ...
 
-  // Add the dependency for the Google services Gradle plugin
-id("com.google.gms.google-services") version "4.4.2" apply false
+//   // Add the dependency for the Google services Gradle plugin
+// id("com.google.gms.google-services") version "4.4.2" apply false
 
-}
+// }
 subprojects {
     val newSubprojectBuildDir: Directory = newBuildDir.dir(project.name)
     project.layout.buildDirectory.value(newSubprojectBuildDir)
