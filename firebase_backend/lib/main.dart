9b4ced5fbe729f9b9d2c0d849121e5ca12b5279e
Gulshid Zada <gulshidzada4@gmail.com>
2025-05-29 17:02:37 +0500

    Starting the firebase backend server side database

---
+++ firebase_backend/lib/main.dart
@@ -0,0 +1,56 @@
+
+import 'package:firebase_backend/firebase_options.dart';
+import 'package:firebase_backend/viewModel/fireProvider.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
+import 'package:provider/provider.dart';
+
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp();
+  //firebase Initalization
+  await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override .
+  Widget build(BuildContext context) {
+    return ScreenUtilInit(
+      designSize: Size(360, 690),
+      minTextAdapt: true,
+      splitScreenMode: true,
+      builder: (context, child) {
+        return MultiProvider(
+          providers: [
+            ChangeNotifierProvider(create: (_) => Fireprovider()),
+          ],
+
+          child: Builder(
+            builder: (BuildContext context) {
+              return MaterialApp(
+                debugShowCheckedModeBanner: false,
+                title: 'Firebase Backend',
+                theme: ThemeData(
+                  applyElevationOverlayColor: true,
+                  brightness: Brightness.light,
+                  appBarTheme: AppBarTheme(color: Colors.teal),
+                  primarySwatch: Colors.blue,
+                  textTheme: Typography.englishLike2018.apply(
+                    fontSizeFactor: 1.sp,
+                  ),
+                ),
+
+                // initialRoute: Routesname.signUp,
+                // onGenerateRoute: Routes.generate_Route,
+              );
+            },
+          ),
+        );
+      },
+    );
+  }
+}
