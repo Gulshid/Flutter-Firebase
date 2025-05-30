49356c91f537af04ce8efda11b9f920ad22fdcd3
Gulshid Zada <gulshidzada4@gmail.com>
2025-05-30 21:04:39 +0500

    Route , view, have made

---
+++ firebase_backend/lib/Routes/Routes.dart
@@ -0,0 +1,24 @@
+import 'package:firebase_backend/Routes/Routesname.dart';
+import 'package:firebase_backend/views_/home_view.dart';
+import 'package:flutter/material.dart';
+
+class Routes {
+  static Route<dynamic> generateRoute(RouteSettings Set) {
+    switch (Set.name) {
+      case Routesname.home:
+        {
+          return MaterialPageRoute(
+            builder: (BuildContext context) => HomeView(),
+          );
+        }
+
+      default:
+        {
+          return MaterialPageRoute(
+            builder:
+                (_) => Scaffold(body: Center(child: Text('No Route found'))),
+          );
+        }
+    }
+  }
+}
