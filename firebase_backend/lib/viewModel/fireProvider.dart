df9afad12a49913634cd8b08cf76947a84e1c80d
Gulshid Zada <gulshidzada4@gmail.com>
2025-06-01 17:18:25 +0500

    Code split into view and view model(Business logic)

--- firebase_backend/lib/viewModel/fireProvider.dart
+++ firebase_backend/lib/viewModel/fireProvider.dart
@@ -1,5 +1,53 @@
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 
-class Fireprovider with ChangeNotifier{
-  
+class Fireprovider with ChangeNotifier {
+  final emailController = TextEditingController();
+  final passwordConyroller = TextEditingController();
+
+  bool isLoading = false;
+
+  //Future function when we sign up
+  Future<void> signUp(BuildContext context) async {
+    isLoading = true;
+    notifyListeners();
+
+    try {
+      UserCredential userid = await FirebaseAuth.instance
+          .createUserWithEmailAndPassword(
+            email: emailController.text,
+            password: passwordConyroller.text,
+          );
+
+      ScaffoldMessenger.of(context).showSnackBar(
+        SnackBar(
+          backgroundColor: Colors.green,
+          content: Text('Sign up Successfully created--->'),
+        ),
+      );
+
+      // ignore: avoid_print
+      print('User ID : ${userid.user?.uid}');
+    } catch (e) {
+      // ignore: avoid_print
+      print('Sign up error--------> $e');
+      ScaffoldMessenger.of(context).showSnackBar(
+        SnackBar(
+          backgroundColor: Colors.red,
+          content: Text('Account creation issues-->'),
+        ),
+      );
+    } finally {
+      isLoading = false;
+      notifyListeners();
+    }
+  }
+
+  //function for dispose all controllers
+  @override
+  void dispose() {
+    emailController.dispose();
+    passwordConyroller.dispose();
+    super.dispose();
+  }
 }
--- firebase_backend/lib/views_/SignUp_view.dart
+++ firebase_backend/lib/views_/SignUp_view.dart
@@ -1,10 +1,8 @@
-// ignore: file_names
-// ignore_for_file: empty_catches
-
-import 'package:firebase_auth/firebase_auth.dart';
+import 'package:firebase_backend/viewModel/fireProvider.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:flutter_spinkit/flutter_spinkit.dart';
+import 'package:provider/provider.dart';
 
 class SignupView extends StatefulWidget {
   const SignupView({super.key});
@@ -14,12 +12,9 @@ class SignupView extends StatefulWidget {
 }
 
 class _SignupViewState extends State<SignupView> {
-  final emailController = TextEditingController();
-  final passwordConyroller = TextEditingController();
-
-  bool isLoading = false;
   @override
   Widget build(BuildContext context) {
+    final Profire = Provider.of<Fireprovider>(context);
     return Scaffold(
       resizeToAvoidBottomInset: false,
       body: Column(
@@ -29,7 +24,7 @@ class _SignupViewState extends State<SignupView> {
           Padding(
             padding: const EdgeInsets.all(8.0),
             child: TextField(
-              controller: emailController,
+              controller: Profire.emailController,
               style: TextStyle(color: Colors.black),
               keyboardType: TextInputType.emailAddress,
               decoration: InputDecoration(
@@ -49,7 +44,7 @@ class _SignupViewState extends State<SignupView> {
             child: TextField(
               obscureText: true,
               obscuringCharacter: '*',
-              controller: passwordConyroller,
+              controller: Profire.passwordConyroller,
               style: TextStyle(color: Colors.black),
               keyboardType: TextInputType.visiblePassword,
               decoration: InputDecoration(
@@ -65,48 +60,8 @@ class _SignupViewState extends State<SignupView> {
 
           SizedBox(height: 30.sp),
           GestureDetector(
-            onTap: () async {
-              setState(() {
-                isLoading = true;
-              });
-              try {
-                UserCredential userid = await FirebaseAuth.instance
-                    .createUserWithEmailAndPassword(
-                      email: emailController.text,
-                      password: passwordConyroller.text,
-                    );
+            onTap: () =>Profire.signUp(context),
 
-                ScaffoldMessenger.of(context).showSnackBar(
-                  SnackBar(
-                    backgroundColor: Colors.green,
-                    content: Text('Sign up Successfully created--->'),
-                  ),
-                );
-
-                // ignore: avoid_print
-                print('User ID : ${userid.user?.uid}');
-
-                setState(() {
-                  isLoading = false;
-                });
-              } catch (e) {
-                // ignore: avoid_print
-                print('Sign up error--------> $e');
-                ScaffoldMessenger.of(context).showSnackBar(
-                  SnackBar(
-                    backgroundColor: Colors.red,
-                    content: Text('Account creation issues-->'),
-                  ),
-                );
-                setState(() {
-                  isLoading = false;
-                });
-              }finally{
-                setState(() {
-                  isLoading = false;
-                });
-              }
-            },
             child: Container(
               height: 50.h,
               width: 200.w,
@@ -115,12 +70,18 @@ class _SignupViewState extends State<SignupView> {
                 borderRadius: BorderRadius.circular(20.r),
               ),
 
-              child:isLoading?  SpinKitFadingCube(size: 25.sp, color: Colors.orange,)  :  Center(
-                child: Text(
-                  'Sign Up',
-                  style: TextStyle(color: Colors.black, fontSize: 20.sp),
-                ),
-              ),
+              child:
+                  Profire.isLoading
+                      ? SpinKitFadingCube(size: 25.sp, color: Colors.orange)
+                      : Center(
+                        child: Text(
+                          'Sign Up',
+                          style: TextStyle(
+                            color: Colors.black,
+                            fontSize: 20.sp,
+                          ),
+                        ),
+                      ),
             ),
           ),
         ],
